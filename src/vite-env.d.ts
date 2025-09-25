e753265beb45c13dd472756a9c48aa505983e154
MrLuke1618 <byebye1618@gmail.com>
2025-09-25 14:31:09 +0700

    fix errors 2

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="vite/client" />
-
-// FIX: Correctly type `process.env.API_KEY` by augmenting the existing NodeJS.ProcessEnv interface.
-// This avoids redeclaring the global `process` variable which causes type conflicts.
-declare namespace NodeJS {
-  interface ProcessEnv {
-    API_KEY: string;
-  }
-}
